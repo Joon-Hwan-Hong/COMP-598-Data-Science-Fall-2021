198f9f8e3d2f2ee1767d4e04190891b651302abc
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2021-10-28 16:30:32 -0400

    unit tests now pass for incorrect formats

--- HW5/hw5/submission_template/test/test_clean.py
+++ HW5/hw5/submission_template/test/test_clean.py
@@ -24,47 +24,36 @@ class CleanTest(unittest.TestCase):
 
     def test_title(self):
         with open(self.fixture1, 'r', encoding='utf-8') as f:
-            print('Checking if title or title_text exists...')          # assumption that JSON data is a dictionary
             data = json.load(f)
-            self.assertTrue(('title' in data) or ('title_text' in data))
-            print('OK')
+            self.assertFalse(('title' in data) or ('title_text' in data))
 
     def test_date(self):
         with open(self.fixture2, 'r', encoding='utf-8') as f:
-            print('Checking if date is in "Date and Time in UTC" ISO 8601 format...')
-            data = json.load(f)                         # assert it can be converted with exact ISO 8601 format
-            self.assertIsInstance(datetime.strptime(data['createdAt'], '%Y-%m-%dT%H:%M:%S%z'), datetime)
-            print('OK')
+            data = json.load(f)
+            with self.assertRaises(ValueError):
+                self.assertIsInstance(datetime.strptime(data['createdAt'], '%Y-%m-%dT%H:%M:%S%z'), datetime)
 
     def test_valid(self):
         with open(self.fixture3, 'r', encoding='utf-8') as f:
-            print('Checking if JSON file is a valid dictionary...')
-            self.assertIsInstance(ast.literal_eval(f.readline()), dict)     # check if raw line is literally a dict
-            print('OK')
+            self.assertNotIsInstance(f.readline(), dict)     # check if raw line is literally a dict
 
     def test_author(self):
         with open(self.fixture4, 'r', encoding='utf-8') as f:
-            print('Checking if author is null, N/A or empty...')
             data = json.load(f)
-            self.assertTrue('author' in data)                                       # author needs to exist as a key
-            self.assertTrue(data['author'] not in ('', 'N/A', 'null', None))        # can't be empty, N/A or null
-            print('OK')
+            self.assertTrue((data['author'] in ('', 'N/A', 'null', None)) or ('author' not in data))
 
     def test_count(self):
         with open(self.fixture5, 'r', encoding='utf-8') as f:
-            print('Checking if total_count is a str containing a castable number (int or float)...')
             data = json.load(f)
             self.assertIsInstance(data['total_count'], (int, float, str))
-            self.assertIsInstance(int(float(data['total_count'])), int)     # check if would be casted into an int
-            print('OK')
+            with self.assertRaises(ValueError):
+                self.assertIsInstance(int(data['total_count']), int)
 
     def test_tags(self):
         with open(self.fixture6, 'r', encoding='utf-8') as f:
-            print('Checking if tag field gets split when given 3 words...')     # assumption that 'tags' exist in JSON
             data = json.load(f)
-            for word in data['tags']:                       # for each word in list, see if str.split() length is 1
-                self.assertEqual(len(word.split()), 1)
-            print('OK')
+            with self.assertRaises(AssertionError):
+                self.assertEqual(len(data['tags'][0].split()), 1)
 
 
 if __name__ == '__main__':
