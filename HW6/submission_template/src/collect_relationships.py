64b79f7a3294129ae4d385ad618f7d0a520589b8
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2021-10-29 18:40:35 -0400

    initialize, no functionality

---
+++ HW6/submission_template/src/collect_relationships.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+DOCSTRING
+"""
+
+# header metadata
+__author__ = 'Joon Hwan Hong'
+__version__ = '1.0'
+__maintainer__ = 'Joon Hwan Hong'
+__email__ = 'joon.hong@mail.mcgill.ca'
+
+# imports
+from bs4 import BeautifulSoup
+
+
+# functions
+def main():
+    #TODO: implement this & watch whatever this lecture they're referring to
+    pass
+
+
+if __name__ == "__main__":
+    main()
