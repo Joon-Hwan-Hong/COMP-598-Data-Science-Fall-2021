5d9bf221f9b84c1795edf46d2159ba5b3791ca32
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2021-11-17 00:49:29 -0500

    simplified script

--- HW8/submission_template/src/compute_pony_lang.py
+++ HW8/submission_template/src/compute_pony_lang.py
@@ -33,13 +33,11 @@ def get_tf_idfs(tf_all, list_p, num_words):
     dic_out = {}
     for pony in list_p:
         list_w_tf = []
-        # make tuple of all words and its tf-idf, then make df and sort by tf-idf value.
         for word in tf_all[pony].keys():
             tf_idf = tf_all[pony][word] * log10(len(list_p) / len([0 for p in list_p if word in tf_all[p].keys()]))
             list_w_tf.append((word, tf_idf))
-        # make temp df to sort by tf-idf values given a pair of word and tf-idf ([0] and [1] respectively)
-        tmp = pd.DataFrame(list_w_tf).sort_values(by=[1], ascending=False)
-        dic_out[pony] = tmp[0].to_list()[0:num_words]
+        # sort by tf-idf values given a pair of word and tf-idf ([0] and [1] respectively)
+        dic_out[pony] = pd.DataFrame(list_w_tf).sort_values(by=[1], ascending=False)[0].to_list()[0:num_words]
 
     return dic_out
 
