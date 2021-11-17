ec79f3a3c3743e0ae66b9f7146f3ee66a00e0ca2
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2021-11-17 00:29:00 -0500

    refactored into functions

--- HW8/submission_template/src/compile_word_counts.py
+++ HW8/submission_template/src/compile_word_counts.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-DOCSTRING
-fixed logic, decided to just split after removing the specified punctuation then drop all. (so things like '"the' is
-removed.
-"""
-
 # header metadata
 __author__ = 'Joon Hwan Hong'
 __version__ = '1.0'
@@ -29,6 +23,18 @@ def get_args():
     return args.output_file, args.data_file
 
 
+def get_files(file_data, dir_stop, list_p, no_punc):
+    # dialog df
+    df = pd.read_csv(file_data, encoding='utf-8')[['pony', 'dialog']]
+    df = df[df['pony'].str.lower().isin(list_p)]
+    df['dialog'] = df['dialog'].str.lower().str.translate(no_punc).str.split()
+    # list_stop
+    with open(dir_stop) as file_stop:
+        list_stop = file_stop.read().splitlines()[6:]
+
+    return df, list_stop
+
+
 def get_list_words(df, list_stop):
     # flatten list-of-lists to list, apply stop words and strip filter.
     list_diag = df['dialog'].to_list()
@@ -39,6 +45,15 @@ def get_list_words(df, list_stop):
     return wc.index.tolist()
 
 
+def get_wcs(list_p, list_words, df):
+    wordcount_p = {}
+    for pony in list_p:
+        list_d = df[df['pony'].str.lower() == pony]['dialog'].to_list()
+        words_p = [word for dialog in list_d for word in dialog if word in list_words]
+        wordcount_p[pony] = pd.Series(words_p).value_counts().to_dict()
+    return wordcount_p
+
+
 def save_json(wordcount_p, file_output):
     with open(file_output, 'w') as file:
         json.dump(wordcount_p, file, indent=4)
@@ -47,24 +62,16 @@ def save_json(wordcount_p, file_output):
 # main block
 def main():
     # init vars
-    wordcount_p = {}
     file_output, file_data = get_args()
     no_punc = str.maketrans('()[],-.?!:;#&', ' ' * 13)
     list_p = ('twilight sparkle', 'applejack', 'rarity', 'pinkie pie', 'rainbow dash', 'fluttershy')
-    with open('../data/stopwords.txt') as file_stop:
-        list_stop = file_stop.read().splitlines()[6:]
 
-    # csv --> pandas df
-    df = pd.read_csv(file_data, encoding='utf-8')[['pony', 'dialog']]
-    df = df[df['pony'].str.lower().isin(list_p)]
-    df['dialog'] = df['dialog'].str.lower().str.translate(no_punc).str.split()
+    # get init data
+    df, list_stop = get_files(file_data, '../data/stopwords.txt', list_p, no_punc)
 
     # get individual word counts
     list_words = get_list_words(df, list_stop)
-    for pony in list_p:
-        list_d = df[df['pony'].str.lower() == pony]['dialog'].to_list()
-        words_p = [word for dialog in list_d for word in dialog if word in list_words]
-        wordcount_p[pony] = pd.Series(words_p).value_counts().to_dict()
+    wordcount_p = get_wcs(list_p, list_words, df)
 
     # save to json output
     save_json(wordcount_p, file_output)
