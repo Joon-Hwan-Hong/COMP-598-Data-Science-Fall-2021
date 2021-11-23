59ce54e3844d5ccd952231f84c2448ef99d779b8
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2021-11-22 23:02:00 -0500

    updated logic

--- HW9/submission_template/src/build_interaction_network.py
+++ HW9/submission_template/src/build_interaction_network.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-This could've been a lot simpler but I wanted to play around with the networkx library so I had the unnecessary
-steps of going from df --> dir graph --> undir graph when it could all be done in pandas in reality
-"""
-
-# header metadata
-__author__ = 'Joon Hwan Hong'
-__version__ = '1.0'
-__maintainer__ = 'Joon Hwan Hong'
-__email__ = 'joon.hong@mail.mcgill.ca'
 
 # imports
+import re
 import json
 import argparse
 import pandas as pd
-import networkx as nx
 from collections import Counter
 
 
@@ -30,80 +20,66 @@ def get_args():
     return args.script_input, args.interaction_network
 
 
-def get_a_to_b(df):
-    tmp = df.copy().shift(1) + '|' + df.copy()
-    tmp = tmp.drop(0)
-    # filter: any containing 'others', 'ponies', 'and' & speaking to oneself
-    tmp = tmp[~tmp['pony'].str.lower().str.contains('others|ponies|and|all')]
-    tmp[['pony', 'pony2']] = tmp['pony'].str.split('|', 1, expand=True)
-    tmp = tmp[tmp['pony'] != tmp['pony2']]
+def get_a_to_b(dic_df, list_episodes):
+    df_return = {}
+    regex = re.compile(r'(?:\W|^)others(?:\W|$)|(?:\W|^)ponies(?:\W|$)|(?:\W|^)and(?:\W|$)|(?:\W|^)all(?:\W|$)')
+    for episode in list_episodes:
+        tmp = dic_df[episode].copy()
+        tmp['pony2'] = tmp.copy().shift(1)
+        tmp = tmp[['pony2', 'pony']].rename(columns={'pony': 'pony2', 'pony2': 'pony'}).dropna()
+        # filter: any containing 'others', 'ponies', 'and' & speaking to oneself
+        tmp = tmp[~tmp['pony'].str.lower().str.contains(regex)]
+        tmp = tmp[~tmp['pony2'].str.lower().str.contains(regex)]
+        df_return[episode] = tmp[tmp['pony'] != tmp['pony2']]
 
-    return tmp
-
-
-def get_undir_graph(g_d):
-    g_u = g_d.to_undirected()
-    for node in g_d:
-        for nhbr in nx.neighbors(g_d, node): # neighbour
-            if node in nx.neighbors(g_d, nhbr):
-                g_u.edges[node, nhbr]['weight'] = g_d.edges[node, nhbr]['weight'] + g_d.edges[nhbr, node]['weight']
-
-    return g_u
+    return df_return
 
 
-def get_most_frequent_characters(df_edges, num_characters):
-    # get dictionary with total interactions per character
-    counts = {}
-    for row in df_edges.itertuples(index=False):
-        if row[0] in counts:
-            counts[row[0]] += row[2]
-        else:
-            counts[row[0]] = row[2]
-        if row[1] in counts:
-            counts[row[1]] += row[2]
-        else:
-            counts[row[1]] = row[2]
-    # get the top <num_characters> of interaction characters and list of all
-    top_chars = [character for (character, count) in Counter(counts).most_common(num_characters)]
-    all_chars = [character for (character, count) in Counter(counts).most_common()]
+def get_lists(dic_df, list_ep, num_top=101):
+    list_all_chars = set()
+    for ep in list_ep:
+        set_ep_chars = set(dic_df[ep].pony).union(set(dic_df[ep].pony2))
+        list_all_chars = list_all_chars.union(set_ep_chars)
+    tmp = dict.fromkeys([*list_all_chars], 0)
+    for ep in list_ep:
+        for row in dic_df[ep].itertuples(index=False):
+            tmp[row[0]] += row[2]
+            tmp[row[1]] += row[2]
 
-    return top_chars, all_chars
+    return [*list_all_chars], [character for (character, count) in Counter(tmp).most_common(num_top)]
 
 
-def df_to_json(df_edges, dir_output, list_c, list_a):
-    # construct desired json dictionary structure
-    dic = dict.fromkeys(list_a, {})
-    for row in df_edges.itertuples(index=False):
-        if dic[row[0]] == {}:
-            dic[row[0]] = {row[1]: row[2]}
-        else:
-            dic[row[0]][row[1]] = row[2]
-        if dic[row[1]] == {}:
-            dic[row[1]] = {row[0]: row[2]}
-        else:
-            dic[row[1]][row[0]] = row[2]
-    # cleanup: remove values of 0, remove those not in top <n>; and write to output
-    dic = {p1: {p2: val for p2, val in dic_w0s.items() if val} for p1, dic_w0s in dic.items() if p1 in list_c}
+def get_json(dic_df, list_ep, list_top, dir_output):
+    dic_json = {char: dict.fromkeys(list_top, 0) for char in list_top}
+    for ep in list_ep:
+        for row in dic_df[ep].itertuples(index=False):
+            dic_json[row[0]][row[1]] += row[2]
+            dic_json[row[1]][row[0]] += row[2]
+    dic_json = {char: {other_char: val for other_char, val in dic_json[char].items() if val} for char in list_top}
     with open(dir_output, 'w') as file:
-        json.dump(dic, file, indent=4)
+        json.dump(dic_json, file, indent=4)
 
 
 # main block
 def main():
     # init data
     dir_script, dir_output = get_args()
-    df = pd.read_csv(dir_script, usecols=['pony'],encoding='utf-8')
+    df = pd.read_csv(dir_script, usecols=['title', 'pony'],encoding='utf-8')
 
-    # df {cols= pony1, pony2} --> directed graph --> undirected graph (edge = sum of both direction edges)
-    df = get_a_to_b(df)
-    df['weight'] = df.groupby(['pony', 'pony2'])['pony'].transform('size')
-    g_directed = nx.from_pandas_edgelist(df, 'pony', 'pony2', edge_attr='weight')
-    g_undirected = get_undir_graph(g_directed)
+    # df raw --> df per episode -> df {cols= pony1, pony2} --> df {cols= pony1, pony2, directed weight}
+    dic_df = dict(tuple(df.groupby('title')))
+    dic_df = {ep: dic_df[ep].drop('title', axis=1) for ep in dic_df}
+    list_ep = [*dic_df]
+    dic_df = get_a_to_b(dic_df, list_ep)
+    dic_df = {ep: pd.DataFrame({'wgt': dic_df[ep].groupby(['pony', 'pony2']).size()}).reset_index() for ep in list_ep}
 
-    # graph --> df --> json & filter if not top <n> --> export
-    df_edges = nx.convert_matrix.to_pandas_edgelist(g_undirected)
-    list_characters, list_all = get_most_frequent_characters(df_edges, num_characters=101)
-    df_to_json(df_edges, dir_output, list_characters, list_all)
+    # filter out if either pony or pony2 countains a non-top 101 character
+    list_all, list_top = get_lists(dic_df, list_ep)
+    dic_df = {ep: dic_df[ep][dic_df[ep]['pony'].isin(list_top)] for ep in list_ep}
+    dic_df = {ep: dic_df[ep][dic_df[ep]['pony2'].isin(list_top)] for ep in list_ep}
+
+    # create json dictionary & export
+    get_json(dic_df, list_ep, list_top, dir_output)
 
 
 if __name__ == "__main__":
